dc778fc782d0f464df1f1a5b762e28ec5e965d94
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-09-12 23:05:56 +0700

    Basic

---
+++ Methods/MaximumMinimum.c
@@ -0,0 +1,23 @@
+int max(int num1, int num2)
+{
+    if (num1 >= num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+}
+
+int min(int num1, int num2)
+{
+    if (num1 <= num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+}
