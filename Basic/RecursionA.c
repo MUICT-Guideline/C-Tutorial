0221f3e291d99ed9e1e1bfa3a0c565c4d4074ed9
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-11-10 09:37:58 +0700

    Added more description

--- Basic/RecursionA.c
+++ Basic/RecursionA.c
@@ -12,10 +12,23 @@ int power(int num, int p)
     }
 }
 
+int factorial(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * factorial(num - 1);
+    }
+}
+
 int main()
 {
-    printf("%d ", power(3, 2)); // 9
-    printf("%d ", power(3, 3)); //27
-    printf("%d ", power(2, 4)); // 16
+    printf("%d \n", power(3, 2)); // 9
+    printf("%d \n", power(3, 3)); //27
+    printf("%d \n", power(2, 4)); // 16
+    printf("%d ", factorial(5));  // 2
     return 0;
 }
